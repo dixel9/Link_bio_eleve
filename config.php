15aae82f9a79dea11f4ffecc951abd07a4853b9b
unknown <diogononatoux@gmail.com>
2024-11-26 11:52:57 -0300

    first

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '54i03Abyp+h5';
+    $dbName = 'bd_eleve';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//    if($conexao->connect_errno)
+//    {
+//        echo "Erro";
+//    }
+//    else
+//    {
+//        echo "Conexão efetuada com sucesso!";
+//    }
+
+?>
